ada544093400b3333b4e859817638106be732ca7
Peter K <pkjar16@student.aau.dk>
2019-03-13 15:15:27 +0100

    motherfuk

---
+++ Shitassa/VisitorExample/ItemElement.java
@@ -0,0 +1,5 @@
+package visitor;
+interface ItemElement
+{
+    public int accept(ShoppingCartVisitor visitor);
+}
---
+++ Shitassa/VisitorExample/ShoppingCartVisitor.java
@@ -0,0 +1,6 @@
+package visitor;
+interface ShoppingCartVisitor
+{
+    int visit(Book book);
+    int visit(Fruit fruit);
+} 
