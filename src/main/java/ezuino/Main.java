bbc788b26f9b0a08ea74796379dc6f534459d1e8
Jonas Andersen <jonas@anrsn.me>
2019-03-14 11:06:40 +0100

    Made Main.java more acceptable

--- src/main/java/ezuino/Main.java
+++ src/main/java/ezuino/Main.java
@@ -3,12 +3,12 @@ import org.antlr.v4.gui.TreeViewer;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.RuleContext;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 import lexer.EzuinoLexer;
 import parser.EzuinoParser;
-
 import javax.swing.*;
 import java.util.Arrays;
 
@@ -21,7 +21,6 @@ public class Main
                         "int b " +
                         "a := 5 " +
                         "b := a + 3.2" +
-                        "prints(a)" +
                         "print(b)");
 
         EzuinoLexer lLexer = new EzuinoLexer(cs);
@@ -35,10 +34,37 @@ public class Main
         ParseTreeWalker walker = new ParseTreeWalker();
 
         walker.walk(baseListener, parseTree);
+        
+        System.out.println("Has this many children : " + parseTree.getChildCount());
 
-        System.out.println(parseTree.toStringTree(parser));
-        showCST(parseTree, parser);
+        // Starts at leaf
+        if (parseTree.getChildCount() == 0) {
+           System.out.println("has 0 children");             
+        } // Parent before leaf
+         else if (parseTree.getChildCount() == 2) {
+            System.out.println("has 1 children");
+            parseTree = parseTree.getChild(0);
+            System.out.println("Has this many children : " + parseTree.getChildCount());
+            System.out.println(parseTree.setParent(RuleContext );
+            //System.out.println(parseTree.getChild(0).getChild(0));  
+        }
+        // multiple parents before leaf
+         else if (parseTree.getChildCount() > 1) {
+            System.out.println("more than 1 ");
+        }
 
+        //System.out.println(parseTree.toStringTree(parser));
+
+
+       showCST(parseTree, parser);
+
+    }
+
+    public void traverse(ParseTree child){ // post order traversal
+        for(ParseTree each : child.getChild()){
+            traverse(each);
+        }
+        this.printData();
     }
 
     private static void showCST(ParseTree parseTree, EzuinoParser parser)
