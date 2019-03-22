8379772f86676b446cceeb0be5714a26a330af86
Peter K <pkjar16@student.aau.dk>
2019-03-22 13:14:34 +0100

    fixed errors and refactor

--- src/main/java/ast/Assign_stmtNode.java
+++ src/main/java/ast/Assign_stmtNode.java
@@ -1,4 +1,11 @@
 package ast;
 
+import ezuino.AstVisitor;
+
 public class Assign_stmtNode extends AstNode {
+    @Override
+	public void accept(AstVisitor v) {
+		v.visit(this);
+		
+	}
 }
--- src/main/java/ast/Func_defNode.java
+++ src/main/java/ast/Func_defNode.java
@@ -1,5 +1,13 @@
 package ast;
 
+import ezuino.AstVisitor;
+
 public class Func_defNode extends AstNode {
+
+	@Override
+	public void accept(AstVisitor v) {
+		v.visit(this);
+		
+	}
 
 }
