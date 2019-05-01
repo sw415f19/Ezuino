66e8c2e3743e024dbabf0fabce6fe8948a62d428
Cklein17 <cklein17@student.aau.dk>
2019-05-01 14:50:08 +0200

    Update CCodeGenerationVisitor.java

--- src/main/java/astvisitors/CCodeGenerationVisitor.java
+++ src/main/java/astvisitors/CCodeGenerationVisitor.java
@@ -309,8 +309,7 @@ public class CCodeGenerationVisitor extends AstVisitor {
     @Override
     public void visit(PrintNode node) {
         out.print("printf(%s, ");
-        for (Iterator<AExpr> iterator = node.getParameters().iterator(); iterator.hasNext(); ) {
-            AExpr exp = iterator.next();
+        for (AExpr exp : node.getParameters()) {
             exp.accept(this);
         }
         out.print(");\n");
