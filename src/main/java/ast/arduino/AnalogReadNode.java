258ae451d0fbcd6400e153c0aa34e6ed44c23fff
Cklein17 <cklein17@student.aau.dk>
2019-05-08 14:53:45 +0200

    Cleaned up imports

--- src/main/java/ast/arduino/AnalogReadNode.java
+++ src/main/java/ast/arduino/AnalogReadNode.java
@@ -2,10 +2,8 @@ package ast.arduino;
 
 import ast.expr.Func_callExprNode;
 import ast.expr.aexpr.AExpr;
-import ast.funcallstmt.Func_callStmtNode;
 import astvisitors.AstVisitor;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class AnalogReadNode extends Func_callExprNode {
